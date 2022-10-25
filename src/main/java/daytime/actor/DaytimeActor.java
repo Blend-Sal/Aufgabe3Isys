7c050c2aa4254630efd6720dc6783c49286a3f83
348364 <igor.greszta@stud.th-luebeck.de>
2022-10-25 16:34:17 +0200

    Verbesserung der Methoden

--- src/main/java/daytime/actor/DaytimeActor.java
+++ src/main/java/daytime/actor/DaytimeActor.java
@@ -4,11 +4,10 @@ import java.util.Date;
 
 import actor.AbstractActor;
 import actor.Actor;
-import actor.ActorReaderWriter;
 
-import static actor.ActorReaderWriter.actorReaderWriter;
-
+import actor.AskStream;
 import fpinjava.Result;
+import inout.ConsoleWriter;
 
 
 class Daytime extends AbstractActor<String> {
@@ -17,8 +16,8 @@ class Daytime extends AbstractActor<String> {
         super(id, type);
     }
 
-    // Reaktion auf eine beliebige Textnachricht die
-    // aktuelle Systemzeit in Form einer Zeichenkette an den Absender zurücksendet.
+    // Reaktion auf eine beliebige Textnachricht:
+    // Ausgeben der aktuellen Systemzeit
     @Override
     public void onReceive(String message, Result<Actor<String>> sender) {
 
@@ -27,8 +26,8 @@ class Daytime extends AbstractActor<String> {
 
     public static void main(String[] args) throws InterruptedException {
         Daytime daytime = new Daytime("daytime", Type.SERIAL);
-        ActorReaderWriter arw = actorReaderWriter("zeit", daytime, 500);
-
+        ConsoleWriter output = ConsoleWriter.stdout();
+        AskStream.ask(daytime, "zeit", 500);
 
     }
 
--- src/test/java/echo/actor/EchoActorTest.java
+++ src/test/java/echo/actor/EchoActorTest.java
@@ -1,19 +1,21 @@
 package echo.actor;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-//import static actor.AskWithOutput.ask;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static list.List.list;
 
-public  class EchoActorTest {
+import actor.Actor;
+import actor.AskStream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import stream.Stream;
 
-	@ParameterizedTest
-	@CsvSource({"'Hallo'",
-							"'HalloHallo'"})
-	public void test(String s) throws InterruptedException {
+public class EchoActorTest {
 
-		Thread.sleep(500);
-    assertEquals("bla","bla");
-	}
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Moin", "Hello", "bye" })
+    public void echoActorTest (String in) throws InterruptedException {
+        Stream<String> askStr = AskStream.ask(new EchoActor("1", Actor.Type.SERIAL),in,100);
+        assertEquals(in, askStr.head());
+    }
+
 }
