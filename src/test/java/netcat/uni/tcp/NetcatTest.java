5c330d8463ad6238fe2fe37db22ede7dfc20b6d0
348364 <igor.greszta@stud.th-luebeck.de>
2022-12-13 20:57:22 +0100

    Änderung jar Dateiname für UniNetcatTest

--- src/test/java/netcat/uni/tcp/NetcatTest.java
+++ src/test/java/netcat/uni/tcp/NetcatTest.java
@@ -11,8 +11,10 @@ import static inout.ProcessWriter.processWriter;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NetcatTest {
-    ProcessBuilder serverbuilder = new ProcessBuilder("java", "-jar", "vspraktikum/wise22_23/Igor.Greszta/aufgaben-ab-0/out/artifacts/Praktikum_jar/UniNetcat.jar", "-l", "5555");
-    ProcessBuilder clientbuilder = new ProcessBuilder("java", "-jar", "vspraktikum/wise22_23/Igor.Greszta/aufgaben-ab-0/out/artifacts/Praktikum_jar/UniNetcat.jar", "localhost", "5555");
+    //ProcessBuilder serverbuilder = new ProcessBuilder("java", "-jar", "vspraktikum/wise22_23/Igor.Greszta/aufgaben-ab-0/out/artifacts/Praktikum_jar/UniNetcat.jar", "-l", "5555");
+    //ProcessBuilder clientbuilder = new ProcessBuilder("java", "-jar", "vspraktikum/wise22_23/Igor.Greszta/aufgaben-ab-0/out/artifacts/Praktikum_jar/UniNetcat.jar", "localhost", "5555");
+    ProcessBuilder serverbuilder = new ProcessBuilder("java", "-jar", "C:\\IntelliJ_Projects\\VSys\\out\\artifacts\\Praktikum_jar\\Praktikum.jar", "-l", "5555");
+    ProcessBuilder clientbuilder = new ProcessBuilder("java", "-jar", "C:\\IntelliJ_Projects\\VSys\\out\\artifacts\\Praktikum_jar\\Praktikum.jar", "localhost", "5555");
 
     @ParameterizedTest
     @ValueSource(strings = {"Nachrichten", "von", "Client", "zu", "Server"})
