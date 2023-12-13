587eeaaaef3faaecbd4dfbce43788a0de1599728
Igor Greszta <igor.greszta@stud.th-luebeck.de>
2023-12-13 21:23:42 +0100

    Fixed wrong location indication in Lidl.json

--- Isysaufg3/src/main/java/Learner.java
+++ Isysaufg3/src/main/java/Learner.java
@@ -1,36 +1,35 @@
-import weka.classifiers.bayes.NaiveBayes;
-import weka.core.Attribute;
-import weka.core.Instances;
-import weka.core.converters.ConverterUtils.DataSource;
-
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
-//TODO WekaPackageManager.loadPackages(false);
 
 public class Learner {
     private final List<SensorData> trainData;
-    private DataSource trainingDataSource;
-    private NaiveBayes naiveBayes;
-    private Instances instances;
-
 
     private Learner(List<SensorData> trainData) {
         this.trainData = trainData;
-    }
-
-    public Learner() {
-        this.trainData = new LinkedList<>();
     }
 
     public static Learner learner(List<SensorData> trainData) {
         return new Learner(trainData);
     }
 
-    public int[][] fileTable(int columns, int rows) {
-        int[][] fileTable = new int[columns][3];
+    public List<SensorData> getGData() {
+        return this.trainData.stream().filter(SensorData::isFavorable).toList();
+    }
 
+    public List<SensorData> getUData() {
+        List<SensorData> data = new ArrayList<>(this.trainData);
+        data.removeAll(getGData());
+        return data;
+    }
+
+    public static int[][] frequencyTable(List<SensorData> dataList) {
+        int[][] frequencyTable = new int[3][3];
+
+        /*
+        mm  mn  mh
+        nm  nn  nh
+        hm  hn  hh
+         */
         int mm = 0;
         int mn = 0;
         int mh = 0;
@@ -40,18 +39,18 @@ public class Learner {
         int hm = 0;
         int hn = 0;
         int hh = 0;
-        for (int i = 0; i < trainData.size(); i++) {
+        for (int i = 0; i < dataList.size(); i++) {
 
-
-            for (int j = 0; j < trainData.get(i).getSequence().size() - 1; j++) {
-                String current = trainData.get(i).getSequence().get(j);
-                String next = trainData.get(i).getSequence().get(j + 1);
+            for (int j = 0; j < dataList.get(i).getSequence().size() - 1; j++) {
+                String current = dataList.get(i).getSequence().get(j);
+                String next = dataList.get(i).getSequence().get(j + 1);
                 if (current.equals("m")) {
                     switch (next) {
                         case "m" -> mm++;
                         case "n" -> mn++;
                         case "h" -> mh++;
                     }
+
                 } else if (current.equals("n")) {
                     switch (next) {
                         case "m" -> nm++;
@@ -64,43 +63,51 @@ public class Learner {
                         case "n" -> hn++;
                         case "h" -> hh++;
                     }
+
+
                 } else {
                     System.out.println("Problem");
                 }
 
             }
 
+            frequencyTable[0][0] = mm;
+            frequencyTable[0][1] = mn;
+            frequencyTable[0][2] = mh;
+            frequencyTable[1][0] = nm;
+            frequencyTable[1][1] = nn;
+            frequencyTable[1][2] = nh;
+            frequencyTable[2][0] = hm;
+            frequencyTable[2][1] = hn;
+            frequencyTable[2][2] = hh;
         }
-        fileTable[0][0] = mm;
-        fileTable[0][1] = mn;
-        fileTable[0][2] = mh;
-        fileTable[1][0] = nm;
-        fileTable[1][1] = nn;
-        fileTable[1][2] = nh;
-        fileTable[2][0] = hm;
-        fileTable[2][1] = hn;
-        fileTable[2][2] = hh;
 
-
-        return fileTable;
+        return frequencyTable;
     }
 
-    public void train(List<SensorData> trainData) throws Exception {
-        ArrayList<Attribute> attributes = new ArrayList<>();
-        for (SensorData dataRow : trainData) {
-            attributes.add(new Attribute("favorability", dataRow.getLabel()));
-            attributes.add(new Attribute("sequence", dataRow.getSequence()));
+    public static double[][] probabilityTable(Learner learner, boolean favorable) {
+        double[][] probabilityTable = new double[3][3];
+        int[][] frequencyTable = favorable ? frequencyTable(learner.getGData()) : frequencyTable(learner.getUData());
+        for (int i = 0; i < frequencyTable.length; i++) {
+            int sum = frequencyTable[i][0] + frequencyTable[i][1] + frequencyTable[i][2];
+            for (int j = 0; j < frequencyTable[i].length; j++) {
+                probabilityTable[i][j] = (float) frequencyTable[i][j] / sum;
+            }
         }
-        this.instances = new Instances("Agent", attributes, 0);
-        this.instances.setClassIndex(0);
 
-        this.naiveBayes = new NaiveBayes();
-        this.naiveBayes.buildClassifier(this.instances);
+
+        return probabilityTable;
     }
 
-    public double predict(int position) throws Exception {
+    public static String print3DTable(double[][] arr) {
 
-        return this.naiveBayes.classifyInstance(this.instances.get(position));
+        return String.format("[%.8f, %.8f, %.8f]%n[%.8f, %.8f, %.8f]%n[%.8f, %.8f, %.8f]", (arr[0][0]),  (arr[0][1]),  (arr[0][2]),  (arr[1][0]),  (arr[1][1]),  (arr[1][2]),  (arr[2][0]),  (arr[2][1]), arr[2][2]);
     }
+
+    public static String print3DTable(int[][] arr) {
+
+        return String.format("[%d, %d, %d]%n[%d, %d, %d]%n[%d, %d, %d]", arr[0][0], arr[0][1], arr[0][2], arr[1][0], arr[1][1], arr[1][2], arr[2][0], arr[2][1], arr[2][2]);
+    }
+
 
 }
--- Isysaufg3/src/main/java/Main.java
+++ Isysaufg3/src/main/java/Main.java
@@ -1,11 +1,12 @@
-import java.io.File;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         Learner test = Learner.learner(FileReader.readDataList("Isysaufg3/src/main/resources/train.txt"));
 
-       int[][] fT = test.fileTable(3, 3);
-        System.out.printf("%d, %d, %d\n%d, %d, %d\n%d, %d, %d", fT[0][0], fT[0][1], fT[0][2], fT[1][0], fT[1][1], fT[1][2], fT[2][0], fT[2][1], fT[2][2]);
+
+        System.out.println("Günstig:\n" + Learner.print3DTable(Learner.frequencyTable(test.getGData())));
+        System.out.println("Ungünstig:\n" + Learner.print3DTable(Learner.frequencyTable(test.getUData())));
+        System.out.println("Günstig:\n" + Learner.print3DTable(Learner.probabilityTable(test, true)));
+        System.out.println("Ungünstig:\n" + Learner.print3DTable(Learner.probabilityTable(test, false)));
+
     }
 }
